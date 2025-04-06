c93d21a2b1301290f01f37089e73e4f846819e4c
sebastian-freire <SEBASTIAN.FREIRE@correo.ucu.edu.uy>
2025-04-05 21:15:01 -0300

    mejoras

--- TA22/script.js
+++ TA22/script.js
@@ -2,8 +2,8 @@ const parrafo = document.getElementById("parrafo");
 
 function mostrarTamañoVentana() {
   parrafo.innerHTML = `Tamaño actual de la ventana: 
-                                 Ancho: <strong>${window.innerWidth}px, 
-                                 Alto: <strong>${window.innerHeight}px`;
+                                 Ancho: ${window.innerWidth}px, 
+                                 Alto: ${window.innerHeight}px`;
 }
 
 window.addEventListener("resize", mostrarTamañoVentana);
